88012ff4b799256ca8ba4483065d4bca0cc5f218
Chashmeet Singh <chashmeetsingh@gmail.com>
2017-01-06 01:58:02 +0530

    Initial Commit

---
+++ YouTube Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YouTube Demo
+//
+//  Created by Chashmeet Singh on 06/01/17.
+//  Copyright © 2017 Chashmeet Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
